d784e0c8529ffc114d59c13e235cb8ea7425fef1
Shun Sakai <sorairolake@protonmail.ch>
2022-08-15 18:25:02 +0900

    Initialize Cargo package

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+//
+// Copyright (C) 2022 Shun Sakai
+//
+
+// Lint levels of rustc.
+#![warn(rust_2018_idioms)]
+#![deny(missing_debug_implementations)]
+// Lint levels of Clippy.
+#![warn(clippy::cargo, clippy::nursery, clippy::pedantic)]
+
+fn main() {
+    println!("Hello, world!");
+}
