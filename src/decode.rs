85bfee7342c5c215f2b32d229e8a99401a1daf48
Shun Sakai <sorairolake@protonmail.ch>
2024-02-18 23:51:49 +0900

    Update version to 0.10.5

--- src/decode.rs
+++ src/decode.rs
@@ -25,15 +25,14 @@ fn svg_to_png(data: &[u8]) -> anyhow::Result<Vec<u8>> {
     use anyhow::Context;
     use resvg::{
         tiny_skia::{Pixmap, Transform},
-        usvg::{self, PostProcessingSteps},
+        usvg,
     };
 
     let opt = usvg::Options::default();
 
-    let mut tree = usvg::Tree::from_data(data, &opt)?;
-    tree.postprocess(PostProcessingSteps::default());
+    let tree = usvg::Tree::from_data(data, &opt)?;
 
-    let pixmap_size = tree.size.to_int_size();
+    let pixmap_size = tree.size().to_int_size();
     let mut pixmap = Pixmap::new(pixmap_size.width(), pixmap_size.height())
         .context("could not allocate a new pixmap")?;
     resvg::render(&tree, Transform::default(), &mut pixmap.as_mut());
