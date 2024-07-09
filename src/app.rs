db619c3ca8e9a6beb2085b71ad1c8d98f6ddfa60
Shun Sakai <sorairolake@protonmail.ch>
2024-07-10 00:37:08 +0900

    Update copyright notice

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2023 Shun Sakai
+// SPDX-FileCopyrightText: 2024 Alexis Hildebrandt
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2022 Shun Sakai
+// SPDX-FileCopyrightText: 2024 Alexis Hildebrandt
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
