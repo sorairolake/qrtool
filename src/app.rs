5d5d9094afd5cd118e320512bd95b8e56f75c8b5
Shun Sakai <sorairolake@protonmail.ch>
2024-07-31 10:17:29 +0900

    refactor: Use `Path::as_os_str`

--- src/app.rs
+++ src/app.rs
@@ -160,7 +160,7 @@ pub fn run() -> anyhow::Result<()> {
             }
             Command::Decode(arg) => {
                 let input = match arg.input {
-                    Some(ref path) if path.to_str().unwrap_or_default() != "-" => fs::read(path)
+                    Some(ref path) if path.as_os_str() != "-" => fs::read(path)
                         .with_context(|| format!("could not read data from {}", path.display()))?,
                     _ => {
                         let mut buf = Vec::new();
