698d539614a84fcd5215cd471dce51f39b4eaa83
Shun Sakai <sorairolake@protonmail.ch>
2024-10-28 22:01:12 +0900

    chore: Move lint configs to `Cargo.toml`

--- build.rs
+++ build.rs
@@ -2,13 +2,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-// Lint levels of rustc.
-#![forbid(unsafe_code)]
-#![deny(missing_debug_implementations)]
-#![warn(rust_2018_idioms)]
-// Lint levels of Clippy.
-#![warn(clippy::cargo, clippy::nursery, clippy::pedantic)]
-
 use std::{
     env, io,
     process::{Command, ExitStatus},
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-// Lint levels of rustc.
-#![forbid(unsafe_code)]
-#![deny(missing_debug_implementations)]
-#![warn(rust_2018_idioms)]
 // Lint levels of Clippy.
-#![warn(clippy::cargo, clippy::nursery, clippy::pedantic)]
 #![allow(clippy::multiple_crate_versions)]
 
 mod app;
